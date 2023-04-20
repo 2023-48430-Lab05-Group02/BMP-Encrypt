5f2da6f4672f7c372630729906b703281dd84a9d
Magmamac Lim <xmachd@gmail.com>
2023-04-20 12:12:46 +1000

    Add custom bool type

---
+++ datatypes/bool.h
@@ -0,0 +1,13 @@
+//
+// Created by Macauley Lim on 20/04/2023.
+//
+
+#ifndef __MY_BOOL_H__
+#define __MY_BOOL_H__
+
+typedef enum {
+    false = 0,
+    true = 1
+} bool;
+
+#endif /* __MY_BOOL_H__ */
--- main.c
+++ main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+#include "datatypes/bool.h"
 
 int main(int argc, char* argv[]){
     // Initialize program
+    bool run = true;
 
-    // Print menu
+    // Enter the event loop
+    while (run) {
+        // Print menu
 
-    // Gather user input
+        // Gather user input
 
-    // Execute on user command
-    printf("Hello, World!\n");
+        // Execute on user command
+        printf("Hello, World!\n");
+
+        // If command is quit, then set run to false.
+        run = false;
+    }
+
+    // Shutdown the program.
     return 0;
 }
