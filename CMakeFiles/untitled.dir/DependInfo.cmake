fcbf6b99d974b36f21d5061ebc364b4006a8159a
Macauley Lim <xmachd@gmail.com>
2023-04-24 16:34:00 +1000

    Add makefile from CMake

---
+++ CMakeFiles/untitled.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mac/Code/BMP-Encrypt/src/bmp.c" "CMakeFiles/untitled.dir/src/bmp.c.o" "gcc" "CMakeFiles/untitled.dir/src/bmp.c.o.d"
+  "/home/mac/Code/BMP-Encrypt/src/encryption.c" "CMakeFiles/untitled.dir/src/encryption.c.o" "gcc" "CMakeFiles/untitled.dir/src/encryption.c.o.d"
+  "/home/mac/Code/BMP-Encrypt/src/input.c" "CMakeFiles/untitled.dir/src/input.c.o" "gcc" "CMakeFiles/untitled.dir/src/input.c.o.d"
+  "/home/mac/Code/BMP-Encrypt/src/main.c" "CMakeFiles/untitled.dir/src/main.c.o" "gcc" "CMakeFiles/untitled.dir/src/main.c.o.d"
+  "/home/mac/Code/BMP-Encrypt/src/rle.c" "CMakeFiles/untitled.dir/src/rle.c.o" "gcc" "CMakeFiles/untitled.dir/src/rle.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/src/bmp.c.o"
+  "CMakeFiles/untitled.dir/src/bmp.c.o.d"
+  "CMakeFiles/untitled.dir/src/encryption.c.o"
+  "CMakeFiles/untitled.dir/src/encryption.c.o.d"
+  "CMakeFiles/untitled.dir/src/input.c.o"
+  "CMakeFiles/untitled.dir/src/input.c.o.d"
+  "CMakeFiles/untitled.dir/src/main.c.o"
+  "CMakeFiles/untitled.dir/src/main.c.o.d"
+  "CMakeFiles/untitled.dir/src/rle.c.o"
+  "CMakeFiles/untitled.dir/src/rle.c.o.d"
+  "untitled"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
