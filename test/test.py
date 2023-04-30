7b54c8f995aabb63482df17e2543105ea5c2626f
Magmamac Lim <xmachd@gmail.com>
2023-05-01 00:12:54 +1000

    update test case to pass automatically

--- test/test.py
+++ test/test.py
@@ -1,29 +1,46 @@
-import os
+import subprocess, os
 
 print("---- TEST 1 - REAL EXAMPLES BMPs ----")
 valid_real_examples = os.listdir("./valid-examples")
 for file in valid_real_examples:
     print(f"-- TEST FILE: {file} --")
-    os.system(f"..\\cmake-build-debug-msys2\\untitled.exe --encrypt --input \"../test/valid-examples/{file}\"")
-    input("-- PRESS ENTER FOR NEXT TEST --")
+    out = os.popen(f"..\\cmake-build-debug-msys2\\untitled.exe --encrypt --input \"../test/valid-examples/{file}\"").read()
+    if out.find("Successfully read bmp"):
+        print("-- PASSED TEST --")
+    else:
+        print(f"-- Test Failed --\n{out}")
+        input("-- PRESS ENTER FOR NEXT TEST --")
 
 print("---- TEST 2 - TEST CASE BMPs ----")
 valid_real_examples = os.listdir("./valid")
 for file in valid_real_examples:
     print(f"-- TEST FILE: {file} --")
-    os.system(f"..\\cmake-build-debug-msys2\\untitled.exe --encrypt --input \"../test/valid/{file}\"")
-    input("-- PRESS ENTER FOR NEXT TEST --")
+    out = os.popen(f"..\\cmake-build-debug-msys2\\untitled.exe --encrypt --input \"../test/valid/{file}\"").read()
+    if out.find("Successfully read bmp"):
+        print("-- PASSED TEST --")
+    else:
+        print(f"-- Test Failed --\n{out}")
+        input("-- PRESS ENTER FOR NEXT TEST --")
+
 
 print("-- TEST 3 - QUESTIONABLE BMPs --")
 valid_real_examples = os.listdir("./questionable")
 for file in valid_real_examples:
     print(f"-- TEST FILE: {file} --")
-    os.system(f"..\\cmake-build-debug-msys2\\untitled.exe --encrypt --input \"../test/questionable/{file}\"")
-    input("-- PRESS ENTER FOR NEXT TEST --")
+    out = os.popen(f"..\\cmake-build-debug-msys2\\untitled.exe --encrypt --input \"../test/questionable/{file}\"").read()
+    if out.find("Successfully read bmp"):
+        print("-- PASSED TEST --")
+    else:
+        print(f"-- Test Failed --\n{out}")
+        input("-- PRESS ENTER FOR NEXT TEST --")
 
 print("-- TEST 4 - CORRUPT BMPs --")
 valid_real_examples = os.listdir("./corrupt")
 for file in valid_real_examples:
     print(f"-- TEST FILE: {file} --")
-    os.system(f"..\\cmake-build-debug-msys2\\untitled.exe --encrypt --input \"../test/corrupt/{file}\"")
-    input("-- PRESS ENTER FOR NEXT TEST --")
+    out = os.popen(f"..\\cmake-build-debug-msys2\\untitled.exe --encrypt --input \"../test/corrupt/{file}\"").read()
+    if out.find("Successfully read bmp"):
+        print("-- PASSED TEST --")
+    else:
+        print(f"-- Test Failed --\n{out}")
+        input("-- PRESS ENTER FOR NEXT TEST --")
