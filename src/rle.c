507e71b8742f86484e4dccf15dde801a9cf1fd3d
Benjamin-Hudson4 <131238317+Benjamin-Hudson4@users.noreply.github.com>
2023-05-17 11:48:02 +1000

    Update rle.c
    added rl4 encoding. not the best implementation, only has a resolution of 1 byte instead of 4 bits, but adding a resolution of 4 bits increases complexity greatly

--- src/rle.c
+++ src/rle.c
@@ -67,6 +67,49 @@ result_t rl4_encode(u8_t** data, u32_t length, BMPImageHeader_t* image_header) {
     result.ok = false;
     result.data = "CODE INCOMPLETE";
 
+    int hight_count, width_count, count, shift;
+    unsigned char *output, current_byte;
+    output = (unsigned char*)malloc(sizeof(data) * 2);
+    /* add test to see if output true*/
+
+    u32* location_counter = malloc((sizeof(u32)));
+
+    location_counter = 0;
+
+    for (hight_count = 0; hight_count < (int)image_header->height; hight_count++) {
+        width_count = 0;
+
+        while (width_count < (int)image_header->width) {
+            *current_byte = data[hight_count * (int)image_header->width + width_count];
+            count = 2;
+            shift = 1;
+
+            while (count < 255 && width_count + shift < (int)image_header->width && data[hight_count * (int)image_header
+                    ->width + width_count + shift] == data[hight_count * (int)image_header->width + width_count]) {
+                count++;
+                count++;
+                shift++;
+            }
+
+            /* it would be better to compress unique pixels in absolute mode, as just using encoded mode can increase
+             * the total file size if there is alot of unique pixels, might add later */
+
+            output[*location_counter++] = (unsigned char) count;
+            output[*location_counter++] = current_byte;
+
+            width_count += shift;
+        }
+
+        output[*location_counter++] = 0;
+        output[*location_counter++] = 0;
+    }
+    output[*location_counter++] = 0;
+    output[*location_counter++] = 1;
+
+
+    /* create test to make sure result.test is true */
+    result.data = output;
+
     return result;
 }
 result_t rl4_decode(u8_t** data, u32_t length) {
